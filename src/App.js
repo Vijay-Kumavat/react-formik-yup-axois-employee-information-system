e1dcc8e597ab93f1d8925b11e00aa741b65fc282
Vijay-Kumavat <kumavatvijay776@gmail.com>
2024-04-14 11:10:52 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import ResponsiveAppBar from './Component/ResponsiveAppBar';
+import SignIn from './Component/SignIn';
+import EmployeeForm from './Component/EmployeeForm';
+import SignUp from './Component/SignUp';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path='/EmployeeForm' element={<EmployeeForm/>} />
+        <Route path='/SignUp' element={<SignUp/>} />
+        <Route path='/SignIn' element={<SignIn/>} />
+      </Routes>
     </div>
   );
 }
